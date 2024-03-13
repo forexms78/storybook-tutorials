9096832517f337a8c972740016e7a66c2fad31a6
parkbh <qudgh9709@gmail.com>
2024-03-13 15:09:25 +0900

    :white_check_mark: Redux toolkit 전역관리 추가

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,23 +1,46 @@
-
 import React from 'react';
 
 import Task from './Task';
 import PropTypes from "prop-types";
+import {useDispatch, useSelector} from "react-redux";
+import {updateTaskState} from "../lib/store.js";
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
-    const events = {
-        onPinTask,
-        onArchiveTask,
-    };
+export default function TaskList() {
+
+    const tasks = useSelector((state) => {
+        const tasksInOrder = [
+            ...state.taskbox.tasks.filter((t) => t.state === 'TASK_PINNED'),
+            ...state.taskbox.tasks.filter((t) => t.state !== 'TASK_PINNED'),
+        ];
+
+        const filteredTasks = tasksInOrder.filter(
+            (t) => t.state === 'TASK_INBOX' || t.state === "TASK_PINNED"
+        );
+        return filteredTasks;
+    });
+
+    const { status } = useSelector((state) => state.taskbox);
+
+    const dispatch = useDispatch();
+
+    const pinTask = (value) => {
+        dispatch(updateTaskState({id: value, newTaskState: 'TASK_PINNED'}))
+    }
+
+    const archiveTask = (value) => {
+        dispatch(updateTaskState({id: value, newTaskState: 'TASK_ARCHIVED'}))
+    }
+
     const LoadingRow = (
         <div className="loading-item">
-            <span className="glow-checkbox" />
+            <span className="glow-checkbox"/>
             <span className="glow-text">
         <span>Loading</span> <span>cool</span> <span>state</span>
       </span>
         </div>
     );
-    if (loading) {
+
+    if (status === 'loading'){
         return (
             <div className="list-items" data-testid="loading" key={"loading"}>
                 {LoadingRow}
@@ -29,11 +52,12 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
             </div>
         );
     }
+
     if (tasks.length === 0) {
         return (
             <div className="list-items" key={"empty"} data-testid="empty">
                 <div className="wrapper-message">
-                    <span className="icon-check" />
+                    <span className="icon-check"/>
                     <p className="title-message">할일 목록이 없습니다</p>
                     <p className="subtitle-message">휴식을 취하셔도 좋아요</p>
                 </div>
@@ -41,14 +65,15 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
         );
     }
 
-    const tasksInOrder = [
-        ...tasks.filter((t) => t.state === 'TASK_PINNED'),
-        ...tasks.filter((t) => t.state !== 'TASK_PINNED'),
-    ];
     return (
-        <div className="list-items">
-            {tasksInOrder.map((task) => (
-                <Task key={task.id} task={task} {...events} />
+        <div className="list-items" data-testid="success" key={"success"}>
+            {tasks.map((task) => (
+                <Task
+                    key={task.id}
+                    task={task}
+                    onArchiveTask={(task) => archiveTask(task)}
+                    onPinTask={(task)=> pinTask(task)}
+                />
             ))}
         </div>
     );
